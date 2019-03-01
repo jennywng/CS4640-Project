69a4e0a8ec2ed58e58e5b50b4c5060df559c6c24
Jenny <jrw3mx@virginia.edu>
2019-02-28 20:07:42 -0500

    login functionality

--- js/login.js
+++ js/login.js
@@ -1,3 +1,19 @@
 $('.message a').click(function(){
     $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+    document.getElementById('login-note').innerHTML = "";
  });
+
+
+ function validateForm() {
+     var em = document.loginform.email.value;
+     var pw = document.loginform.pword.value;
+     var email = "jrw3mx@virginia.edu";
+     var password = "password";
+     if ((em == email) && (pw == password)) {
+         window.location = "user-home.html";
+         return true;
+     } else {
+         document.getElementById('login-note').innerHTML = "Username or password incorrect";
+         return false;
+     }
+ }
