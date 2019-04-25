59dab638a332b8a3ef3f0bc4943eacfe4bda4a0f
Jenny <jrw3mx@virginia.edu>
2019-04-24 21:22:08 -0400

    my reviews page done

--- my-reviews/src/app/app.component.ts
+++ my-reviews/src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Bathroom } from './bathroom';
 import { BathroomService } from './bathroom.service';
 
+import {Review} from './reviews'
 
-
-import { Reviewer} from './reviewer';
+import { Reviewer} from './reviewers';
 import { CookieService} from 'ngx-cookie-service';
 // import { CookieService } from 'angular2-cookie/core';
 import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
@@ -24,6 +24,11 @@ export class AppComponent implements OnInit{
   title = 'Flushd';
   myData: Object;
   otherData: Object;
+
+  reviews: string;
+
+  myReviews: Review[] = [];
+
         
   constructor(private bathroomService: BathroomService, private http: HttpClient, private cookieService: CookieService) {
   }
@@ -33,6 +38,7 @@ export class AppComponent implements OnInit{
     let id = this.getUserCookie();
     console.log('id from cookie: ', id);
     this.getMyReviews(id);
+    // this.parseData();
   }
 
   getUserCookie() {
@@ -42,11 +48,32 @@ export class AppComponent implements OnInit{
   getMyReviews(data): void {
     this.http.get('http://localhost/CS4640-Project/php/get-my-reviews.php?uid='+ data).subscribe((data) => {
         console.log("Got data from backend", data);
+        this.reviews = JSON.stringify(data);
+
+        // console.log(JSON.parse(this.reviews));
         this.myData = data;
+
+        let json = JSON.parse(JSON.stringify(data));
+
+        console.log(json);
+
+        json.my_reviews.forEach(e => {
+          let r = new Review();
+          r.id = e.id; r.bID = e.bID; r.uID = e.bID; r.title = e.title; 
+          r.rDesc = e.rDesc; r.rating = e.rating; 
+          if (e.imgURL) {
+            r.img = 'http://localhost/CS4640-Project/' + e.imgURL;
+          } else {
+            r.img = null;
+          }
+          this.myReviews.push(r);
+        });
+        return this.myReviews;
     }, (error) => {
       console.log('Error', error);
     })
   }
+
 
 
   getOtherReview(data): void {
---
+++ my-reviews/src/app/reviews.ts
@@ -0,0 +1,14 @@
+export class Review {
+   public id: number;
+   public bID: number;
+   public uID: number;
+   public title: string;
+   public rDesc: string;
+   public rating: number;
+   public img: string;
+
+    constructor(
+
+    ){}
+ }
+ 
